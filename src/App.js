8cfa5286978b7c5563464c9683e4004328ec7365
Deepak Kamat <dkamat.001@gmail.com>
2022-05-27 20:49:35 +0530

    chore: setup tailwind css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2>Redux toolkit Movie app</h2>
+      <h2 className="text-red-500">Redux toolkit Movie app</h2>
     </div>
   );
 }
