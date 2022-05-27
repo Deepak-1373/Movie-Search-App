bd7ee7c94648b77f85436536007cf7951d4d749c
Deepak Kamat <dkamat.001@gmail.com>
2022-05-27 21:06:57 +0530

    chore: setup redux and other dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "#0f171e",
+        "secondary-color": "#1a242f",
+        "font-primary": "#ffffff",
+        "font-secondary": "#79b8f3",
+      },
+    },
   },
   plugins: [],
 };
