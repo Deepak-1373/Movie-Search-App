95c47840c53bcab417a5f0825ca0504ea8e779af
Deepak Kamat <dkamat.001@gmail.com>
2022-05-28 11:49:43 +0530

    feat: configure redux-toolkit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { movieStore } from "./features/store";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={movieStore}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
