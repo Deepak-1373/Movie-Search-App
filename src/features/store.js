478435dd709f9960fa35c9aef90ce0306baa5306
Deepak Kamat <dkamat.001@gmail.com>
2022-05-28 11:58:35 +0530

    feat: add movieSlice

--- src/features/store.js
+++ src/features/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import moviesReducer from "./movies/movieSlice";
 
 export const movieStore = configureStore({
-  reducer: {},
+  reducer: moviesReducer,
 });
