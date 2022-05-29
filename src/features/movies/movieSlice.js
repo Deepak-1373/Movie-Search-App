0480b7788e350a3d6a5b5f7bd148ba2c20173945
Deepak Kamat <dkamat.001@gmail.com>
2022-05-29 12:42:56 +0530

    feat: add middleware for asynchronous operation

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,4 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { movieApi } from "../../api/movieApi";
+import { movieApiKey } from "../../api/movieAPIKey";
+
+export const fetchAsyncMovies = createAsyncThunk(
+  "movies/fetchAsyncMovies",
+  async () => {
+    const movieText = "Harry";
+    const response = await movieApi.get(
+      `?apikey=${movieApiKey}&s=${movieText}&type=movie`
+    );
+    return response.data;
+  }
+);
 
 const initialState = {
   movies: {},
@@ -11,6 +24,18 @@ const movieSlice = createSlice({
     addMovies: (state, { payload }) => {
       state.movies = payload;
     },
+  },
+  extraReducers: {
+    [fetchAsyncMovies.pending]: () => {
+      console.log("Pending");
+    },
+    [fetchAsyncMovies.fulfilled]: (state, { payload }) => {
+      console.log("Fetched Succesfully");
+      return { ...state, movies: payload };
+    },
+  },
+  [fetchAsyncMovies.rejected]: () => {
+    console.log("Rejected");
   },
 });
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,13 @@
 import React, { useEffect } from "react";
 import { MovieListing } from "../components";
-import { movieApi } from "../api/movieApi";
-import { movieApiKey } from "../api/movieAPIKey";
 import { useDispatch } from "react-redux";
-import { addMovies } from "../features/movies/movieSlice";
+import { fetchAsyncMovies } from "../features/movies/movieSlice";
 
 export const Home = () => {
-  const movieText = "Harry";
   const dispatch = useDispatch();
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await movieApi
-        .get(`?apikey=${movieApiKey}&s=${movieText}&type=movie`)
-        .catch((err) => console.log(err));
-      dispatch(addMovies(response.data));
-    };
-    fetchMovies();
-  }, []);
+    dispatch(fetchAsyncMovies());
+  });
 
   return (
     <div>
