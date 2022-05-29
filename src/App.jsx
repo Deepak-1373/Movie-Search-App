b115881bae1a21cfb8c0f9766f01386fb28dfa1a
Deepak Kamat <dkamat.001@gmail.com>
2022-05-29 13:54:02 +0530

    feat(setup): add movie detail component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Footer, Header } from "./components";
 import { Home } from "./pages/Home";
+import { MovieDetail } from "./pages/MovieDetail";
 import "./index.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <div className="my-0 mx-10">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/movie/:imdbID" element={MovieDetail} />
         </Routes>
       </div>
       <Footer />
