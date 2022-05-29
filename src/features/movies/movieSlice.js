0bf8e8c2b7b90f418782e23fb32ba45200246b30
Deepak Kamat <dkamat.001@gmail.com>
2022-05-29 21:28:46 +0530

    feat: add cleanup function for movie detail page

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -44,8 +44,8 @@ const movieSlice = createSlice({
   name: "movies",
   initialState,
   reducers: {
-    addMovies: (state, { payload }) => {
-      state.movies = payload;
+    removeSelectedMovieOrShow: (state) => {
+      state.selectedMovieOrShow = {};
     },
   },
   extraReducers: {
@@ -70,7 +70,7 @@ const movieSlice = createSlice({
   },
 });
 
-export const { addMovies } = movieSlice.actions;
+export const { removeSelectedMovieOrShow } = movieSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
 export const getAllShows = (state) => state.movies.shows;
 export const getSelectedMovieOrShow = (state) =>
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   fetchAsyncMoviesOrShowsDetails,
   getSelectedMovieOrShow,
+  removeSelectedMovieOrShow,
 } from "../features/movies/movieSlice";
 
 export const MovieDetail = () => {
@@ -13,61 +14,76 @@ export const MovieDetail = () => {
 
   useEffect(() => {
     dispatch(fetchAsyncMoviesOrShowsDetails(imdbID));
+    return () => {
+      dispatch(removeSelectedMovieOrShow());
+    };
   }, [dispatch, imdbID]);
 
   return (
     <div className="flex py-10 px-0 text-font-primary font-normal">
-      <div>
-        <div className="text-4xl text-font-primary">{data.Title}</div>
-        <div className="flex pl-[3px] mt-5 text-font-secondary flex-wrap">
-          <span className="mr-5 mt-3 flex items-center justify-center">
-            IMDB Rating
-            <span className="ml-2 material-symbols-outlined">star</span> :
-            {data.imdbRating}
-          </span>
-          <span className="mr-5 mt-3 flex items-center justify-center">
-            IMDB Votes
-            <span className="ml-2 material-symbols-outlined">thumb_up</span> :
-            {data.imdbVotes}
-          </span>
-          <span className="mr-5 mt-3 flex items-center justify-center">
-            Runtime
-            <span className="ml-2 material-symbols-outlined">videocam</span> :
-            {data.Runtime}
-          </span>
-          <span className="mr-5 mt-3 flex items-center justify-center">
-            Year
-            <span className="ml-2 material-symbols-outlined">
-              calendar_month
-            </span>
-            :{data.Year}
-          </span>
-        </div>
-        <div className="mt-5 leading-7">{data.Plot}</div>
-        <div className="movie-info">
+      {Object.keys(data).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <>
           <div>
-            <span>Director</span>
-            <span>{data.Director}</span>
+            <div className="text-4xl text-font-primary">{data.Title}</div>
+            <div className="flex pl-[3px] mt-5 text-font-secondary flex-wrap">
+              <span className="mr-5 mt-3 flex items-center justify-center">
+                IMDB Rating
+                <span className="ml-2 text-yellow-500 material-symbols-outlined">
+                  star
+                </span>{" "}
+                :{data.imdbRating}
+              </span>
+              <span className="mr-5 mt-3 flex items-center justify-center">
+                IMDB Votes
+                <span className="ml-2 material-symbols-outlined">
+                  thumb_up
+                </span>{" "}
+                :{data.imdbVotes}
+              </span>
+              <span className="mr-5 mt-3 flex items-center justify-center">
+                Runtime
+                <span className="ml-2 material-symbols-outlined">
+                  videocam
+                </span>{" "}
+                :{data.Runtime}
+              </span>
+              <span className="mr-5 mt-3 flex items-center justify-center">
+                Year
+                <span className="ml-2 material-symbols-outlined">
+                  calendar_month
+                </span>
+                :{data.Year}
+              </span>
+            </div>
+            <div className="mt-5 leading-7">{data.Plot}</div>
+            <div className="movie-info">
+              <div>
+                <span>Director</span>
+                <span>{data.Director}</span>
+              </div>
+              <div>
+                <span>Stars</span>
+                <span>{data.Actors}</span>
+              </div>
+              <div>
+                <span>Genres</span>
+                <span>{data.Genre}</span>
+              </div>
+              <div>
+                <span>Languages</span>
+                <span>{data.Language}</span>
+              </div>
+              <div>
+                <span>Awards</span>
+                <span>{data.Awards}</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <span>Stars</span>
-            <span>{data.Actors}</span>
-          </div>
-          <div>
-            <span>Genres</span>
-            <span>{data.Genre}</span>
-          </div>
-          <div>
-            <span>Languages</span>
-            <span>{data.Language}</span>
-          </div>
-          <div>
-            <span>Awards</span>
-            <span>{data.Awards}</span>
-          </div>
-        </div>
-      </div>
-      <img src={data.Poster} alt={data.Title} />
+          <img src={data.Poster} alt={data.Title} />
+        </>
+      )}
     </div>
   );
 };
