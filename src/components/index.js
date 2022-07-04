4a33f70e767e5967a6f8e358783ecae3410a7031
Deepak Kamat <dkamat.001@gmail.com>
2022-07-04 21:10:26 +0530

    feat: add selectBox component for single source of fetching

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { Header } from "./Header";
 export { Footer } from "./Footer";
 export { MovieListing } from "./MovieListing";
 export { MovieCard } from "./MovieCard";
+export { SelectBox } from "./SelectBox";
